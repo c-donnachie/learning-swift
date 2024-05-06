6f7f2b449571519809e6796db6c6344d1e96a942
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2024-05-05 21:52:57 -0400

    Initial Commit

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Donnachie on 05-05-24.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
